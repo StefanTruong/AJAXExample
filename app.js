40ff0d03ae87594ddc42120c3366a58d92a538f3
Stefan Truong <stefan.truong@gmail.com>
2021-11-10 15:47:23 +0100

    add: buggy code

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+
+// const button = document.querySelector('button')
+
+// button.addEventListener('click', ()=> {                                 
+//     setTimeout(() => button.classList.toggle('buttonChanged'), 1000)    // Using two Callback to change color of the button. Doing unneccessary stuff
+//     button.value = 'There you are'
+// })
+
+
+
+
+function showCountries(){
+    // let xhr = new XMLHttpRequest                                // xhr = XmlHttpRequest
+    // xhr.open('GET','https://restcountries.com/v3.1/all', true)  // Request is initialized
+    // xhr.send()                                                  // Actual Object is send
+    // xhr.onload = function() {                                   // what to do on response
+    //     if(xhr.status == 200){
+    //         console.log('You Got a HTTP Status Code 200')
+            // let countriesJSONlist = JSON.parse(this.response)   // Responseobject is actually a JSON file
+            // console.log(countriesJSONlist)
+            // countriesJSONlist.forEach(country => { 
+            //     const singleCountry = document.createElement('div')
+            //     singleCountry.innerHTML = countriesJSONlist.name
+            //     document.getElementById('listDiv').appendChild(singleCountry)
+            // });
+        }
+})
